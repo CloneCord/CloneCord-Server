973f1052c123f6e53696852057a69e19f9ff00e1
LelouBil <bilel.medimegh@gmail.com>
2020-02-11 21:43:28 +0100

    Initial commit

---
+++ src/main/java/net/leloubil/clonecordserver/ClonecordserverApplication.java
@@ -0,0 +1,13 @@
+package net.leloubil.clonecordserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecordserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonecordserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/leloubil/clonecordserver/ClonecordserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leloubil.clonecordserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecordserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
