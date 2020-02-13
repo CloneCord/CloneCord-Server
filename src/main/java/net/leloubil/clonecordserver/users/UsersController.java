f43388ff33336f9454a8bc7195c06ab57ccf639f
LelouBil <bilel.medimegh@gmail.com>
2020-02-13 21:47:10 +0100

    Added GET and PUT for Users
    Closes #2

--- src/main/java/net/leloubil/clonecordserver/users/UsersController.java
+++ src/main/java/net/leloubil/clonecordserver/users/UsersController.java
@@ -4,7 +4,9 @@ import net.leloubil.clonecordserver.data.LoginUser;
 import net.leloubil.clonecordserver.data.User;
 import net.leloubil.clonecordserver.exceptions.UserNotFoundException;
 import net.leloubil.clonecordserver.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,14 +24,20 @@ public class UsersController {
 
     @GetMapping("/{userName}")
     public User getUser(@PathVariable("userName") String username){
-        //todo get user data from database
-        return null;
+        return userService.getUserByName(username).orElseThrow(() -> new UserNotFoundException(null,username));
     }
 
     @GetMapping("/@self")
     public User getSelf(){
         LoginUser loginUser = (LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userService.getUser(loginUser.getUuid()).orElseThrow(() -> new UserNotFoundException(loginUser.getUuid(),null));
+    }
+
+    @PutMapping("/@self")
+    public User putSelf(@Validated @RequestBody User user){
+        LoginUser loginUser = (LoginUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        user.setId(loginUser.getUuid());
+        return userService.updateUser(user);
     }
 
 }
